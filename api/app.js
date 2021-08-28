bdb0215bbb79c79ae9f5fce0cbab0c916bdf05f3
Andrea Tan <andreaxtan611@gmail.com>
2021-08-29 02:33:04 +0800

    Add error handling for post request

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -10,12 +11,15 @@ app.use(express.json()); // application/json
 
 app.use('/feed', feedRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).send({
-    status: 404,
-    error: "Not found"
-    })
-   })
-   
+app.use((error, req, res, next) => {
+   console.log(error);
+   const status = error.statusCode || 500;
+   const message = error.message;
+   res.status(status).json({message: message});
+})
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://Andrea:password<3@cluster0.k1rxn.mongodb.net/task_b1?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(8080);
+}).catch(err => console.log(err));
+   
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,15 +1,6 @@
 const { post } = require("../routes/feed");
 
-// posts = [
-//     {
-//         todo: 'Cook dinner',
-//         id: 1
-//     },
-//     {
-//         todo: 'Do homework',
-//         id: 2
-//     }    
-// ]
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
@@ -30,17 +21,25 @@ exports.getPosts = (req, res, next) => {
 exports.postPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {name: 'Andrea'},
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        creator: {name: 'Andrea'},
+    });
+
+    post.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        })
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
         }
+        console.log(err.statusCode);
+        next(err);
     })
-    posts.push({todo: todo, id: id})
 }
 
 exports.deletePost = (req, res, next) => {
