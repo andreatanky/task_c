eb006f44aa3f4140a138798240540a903aaf6cda
Andrea Tan <andreaxtan611@gmail.com>
2021-08-30 16:17:52 +0800

    Add .travis.yml file

--- api/test/test.js
+++ api/test/test.js
@@ -19,44 +19,80 @@ describe("Post API", function () {
       /**
       * Test POST
       */
-    // describe("POST /feed/post", function () {
-    //     it("Post a post", function () {
-    //         app.post("/feed/post").send({
-    //             title: "Post for testing",
-    //             content: "Content of post for testing",
-    //             creator: {name: 'Testing'},
-    //         }).end((error, response) => {
-    //             response.should.have.status(200);
-    //             expect(response.body.message).to.equal("Post created successfully!");
+    describe("POST /feed/post", function () {
+        it("Post a post", function () {
+            app.post("/feed/post").send({
+                title: "Post for testing!",
+                content: "Content of post for testing!",
+                creator: {name: 'Testing'},
+            }).end((error, response) => {
+                response.should.have.status(200);
+                expect(response.body.message).to.equal("Post created successfully!");
+            })
+        });
+    });
+
+     /**
+      * Test DELETE
+      */
+    //   describe("DELETE request", function() {
+    //     before("Finding a post", async function() {
+    //         let post = await Post.findOne({title: 'Post for testing!'});
+    //         postId = post._id.toString();
+    //     })
+    //     describe("Deleting a post", async function() {
+    //         let result = null;
+    //         before(async function() {
+    //             result = await app.delete(`/feed/post/${postId}`);
+    //             console.log("result: ", result.body);
+    //         });
+    //         it("Should delete the post successfully", function(done) {
+    //             expect(result.body.message).to.equal('Post is successfully deleted.');
+    //             done();
     //         })
-    //     });
-    // });
+    //     })
+    // })
+
+    describe('DELETE /feed/post/:postId', () => {
+        it('it should DELETE a post given the id', (done) => {
+            let post = new Post({title: "Testing delete API", content: "Testing delete API content", creator: {name: 'Testing'}})
+            post.save((err, post) => {
+                  chai.request(server)
+                  .delete('/feed/post/' + post._id)
+                  .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('message').eql('Post is successfully deleted.');
+                    done();
+                  });
+            });
+        });
+    });
 
       /**
       * Test GET
       */
-    // describe("GET /feed/posts", function () {
-    //     it("Get all posts", function () {
-    //         app.get("/feed/posts").end((error, response) => {
-    //             response.should.have.status(200);
-    //             expect(response.body.message).to.equal("Fetched posts successfully.");
-    //         })
-    //     });
-    // });
+    describe("GET /feed/posts", function () {
+        it("Get all posts", function () {
+            app.get("/feed/posts").end((error, response) => {
+                response.should.have.status(200);
+                expect(response.body.message).to.equal("Fetched posts successfully.");
+            })
+        });
+    });
 
     /**
       * Test PUT
       */
 
-    describe("PUT request", function() {
-        before("Finding a post", async function() {
-            let post = await Post.findOne({title: 'Post for testing'});
-            postId = post._id.toString();
-        })
+    describe("PUT /feed/post/:postId", function() {
+        // before("Finding a post", async function() {
+        //     let post = await Post.findOne({title: 'Post for testing'});
+        //     postId = post._id.toString();
+        // })
         describe("Edit a post", async function() {
             let result = null;
             before(async function() {
-                result = await app.put(`/feed/post/${postId}`)
+                result = await app.put(`/feed/post/612a8a4567edec0909b514ea`)
                 .send({
                     title: "Edit post for testing",
                     content: "Edit content for testing",
@@ -69,23 +105,5 @@ describe("Post API", function () {
             })
         })
     })
-
-    /**
-      * Test DELETE
-      */
-     describe("DELETE request", function() {
-        describe("Deleting a post", async function() {
-            let result = null;
-            before(async function() {
-                result = await app.delete(`/feed/post/${postId}`);
-                console.log("result: ", result);
-            });
-            it("Should delete the post successfully", function(done) {
-                expect(result.body.message).to.equal('Post is successfully deleted.');
-                done();
-            })
-        })
-    })
-
 })
 
