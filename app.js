c577a58092c197eae0f1dd8dfbd18a510f9a5c29
Andrea Tan <andreaxtan611@gmail.com>
2021-09-06 22:15:04 +0800

    Update env variables

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var cors = require("cors");
+require('dotenv').config();
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 app.use(cors());
+const pw = process.env.MONGODB_PASSWORD;
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
@@ -20,7 +22,7 @@ app.use((error, req, res, next) => {
    res.status(status).json({message: "An error occured!"});
 })
 
-mongoose.connect('mongodb+srv://Andrea:password<3@cluster0.k1rxn.mongodb.net/task_b1?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Andrea:' + pw + '@cluster0.k1rxn.mongodb.net/task_b1?retryWrites=true&w=majority')
 .then(result => {
     app.listen(8080);
 }).catch(err => console.log(err));
